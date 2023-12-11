f6bbbb69deb190c732e58004ebde88ce3c997e28
Piotr Duda <piotrduda2004y@gmail.com>
2023-12-11 22:38:27 +0100

    test commit

--- Day1/Day1.py
+++ Day1/Day1.py
@@ -1,1 +1,7 @@
 data = open('Day1Input.txt', 'r').read().split('\n')
+for line in data:
+    for key in line:
+        if(key.isdigit()):
+            first = key
+            print(first)
+            break
