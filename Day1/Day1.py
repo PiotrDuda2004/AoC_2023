f47a9bb323bfcd06cc3568ff6d38fa1cfb09f2b3
Piotr Duda <piotrduda2004y@gmail.com>
2023-12-11 22:59:51 +0100

    Day 1 Completed

--- Day1/Day1.py
+++ Day1/Day1.py
@@ -1,8 +1,14 @@
 data = open('Day1Input.txt', 'r').read().split('\n')
+sum = 0
 for line in data:
     for key in line:
         if(key.isdigit()):
-            first = key
-            print(first)
+            first = key # Get first digit character
             break
-            
+    line = line[::-1] #reverse the whole string that we're working on
+    for key in line:
+        if(key.isdigit()):
+                second = key #Get second digit character
+                break
+    sum +=int(first+second) 
+print(sum)
